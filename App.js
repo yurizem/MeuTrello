99ac97176c596e28928152412bd122290b7e8db3
YuriVinicius <yurizem333@gmail.com>
2020-12-23 10:38:13 -0300

    Tela inicial e botão de ad

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, SafeAreaView, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#171d31" barStyle="light-content" />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>Minhas Tarefa</Text>
+      </View>
+
+      <TouchableOpacity style={styles.fab}>
+        <Ionicons 
+        name="ios-add"
+        size={35}
+        color="#FFF"
+        />
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171d31'
+  },
+  content:{
+  },
+  title:{
+    color:'white',
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  fab:{
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#0094FF',
+    borderRadius: 30,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset:{
+      width: 1,
+      height: 3
+    }
+  }
+})
